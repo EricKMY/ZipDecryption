c4dca0edec15433dfb50e9d1ccba1634ea2de88a
EricKuanKMY <erickuan970810edu@gmail.com>
2019-05-16 10:11:04 +0800

    zip decryption test without  parallel

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import zipfile, os
 def main():
     
     filePath = 'test.zip'
-    password = '123'
+    passwordDict = ['100', '111', '5559','151514','555','123']
 
     with zipfile.ZipFile(filePath) as testZip:
-        try:
-            with testZip.open('test.txt',  pwd=bytes(password, 'utf-8')) as testFile:
-                print('password: ' + password)
-                print(testFile.read())
-        except RuntimeError:
-            print('wrong password')
+        for password in passwordDict: 
+            try:
+                with testZip.open('test.txt',  pwd=bytes(password, 'utf-8')) as testFile:
+                    print('password: ' + password)
+                    print(testFile.read())
+                    break
+            except RuntimeError:
+                print('wrong password: ' + password)
 
 print(main())
